21011bcbd6448b63f15031e16e5293a2a4a607c2
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-04-30 16:28:02 -0300

    Add connectio to DB postgres and simple query return with json

--- lib/controller/UserController.dart
+++ lib/controller/UserController.dart
@@ -1,22 +1,30 @@
 import 'dart:async';
+import 'dart:convert';
 
+import 'package:postgres/postgres.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 part 'UserController.g.dart';
 
-class UserController{
-
+class UserController {
   @Route.get('/')
-  FutureOr<Response> getTodos(Request request){
-    return Response.ok('Retorna todos');
+  FutureOr<Response> getTodos(Request request) async {
+    final connection = PostgreSQLConnection('localhost', 4321, 'dados', username: 'postgres', password: '123456');
+    await connection.open();
+
+    List<Map<String, Map<String, dynamic>>> result = await connection.mappedResultsQuery('select * from tb_pessoa as t');
+
+    await connection.close();
+
+    return Response.ok(json.encode(result));
   }
 
   @Route.get('/<userId>')
-  FutureOr<Response> getUser(Request request, String userId){
+  FutureOr<Response> getUser(Request request, String userId) {
     return Response.ok('Retorna usuario: $userId');
   }
 
-  Router get router{
+  Router get router {
     return _$UserControllerRouter(this);
   }
 }
